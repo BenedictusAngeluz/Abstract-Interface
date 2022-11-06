0348f018b4676e12d046020f38e401eef032f490
Benedictus Angelo <100206585+BenedictusAngeluz@users.noreply.github.com>
2022-11-06 21:40:54 +0700

    Java Interface

---
+++ interface/Printer.java
@@ -0,0 +1,18 @@
+public class Printer implements Copier, Scanner {
+    public void scaneImage() {
+        System.out.println("Scanning image...");
+    }
+
+    public void copyImage() {
+        System.out.println("Copy image...");
+    }
+
+    public void printImage() {
+        System.out.println("Print image...");
+    }
+
+    /*
+     * PERBAIKAN PADA LINE 3, 7, DAN 11:
+     * Ganti simbol “ ” menjadi " " agar teks dapat tercetak.
+     */
+}
---
+++ interface/Scanner.java
@@ -0,0 +1,14 @@
+public interface Scanner {
+    public void scaneImage();
+    // {
+    // System.out.println(“Scanning image...”);
+    // }
+
+    /*
+     * PERBAIKAN:
+     * Hilangkan body pada method scaneImage dan tambahkan ; untuk menutup kode,
+     * karena method yang terdapat di abstract class atau interface class harus
+     * tidak memiliki body atau isian. Body atau isian hanya bisa ditambahkan pada
+     * sub class yang telah diwarisi atau diimplementasikan dari super class.
+     */
+}
